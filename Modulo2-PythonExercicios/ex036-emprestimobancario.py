c5720e9c541ad7e29fb8ceeaf88421da29322c25
flavioalessandropereira <flavioalessandropereira@gmail.com>
2023-02-18 21:24:39 -0300

    manutenção do exercício

--- Modulo2-PythonExercicios/ex036-emprestimobancario.py
+++ Modulo2-PythonExercicios/ex036-emprestimobancario.py
@@ -8,7 +8,7 @@ prestacao = valor_casa / meses_pagar
 nao_pode_exceder = salario * 0.30
 
 if nao_pode_exceder < prestacao:
-    print('Os 30% de seu salário é R$ {:.2f} e a prestação é R$ {:.2f}.'.format(nao_pode_exceder, prestacao))
+    print('O valor dos 30% de seu salário é R$ {:.2f} , será destinado a  prestação do financiamento de R$ {:.2f}.'.format(nao_pode_exceder, prestacao))
     print('\033[1;31mEmpréstimo NEGADO\033[m')
 else:
     print('\033[1;34mPARABÉNS!ACABOU DE FINANCIAR UMA CASA.\033[m A prestação \033[1:32m R$ {:.2f}.\033[m'.format(prestacao))
