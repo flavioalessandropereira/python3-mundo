74e1a2d42bf5884f4eb44bc2b5feeed86fba7347
flavioalessandropereira <flavioalessandropereira@gmail.com>
2023-02-24 09:28:48 -0300

    continuando...

--- Modulo2-PythonExercicios/ex036-emprestimobancario.py
+++ Modulo2-PythonExercicios/ex036-emprestimobancario.py
@@ -1,6 +1,6 @@
-valor_casa = float(input('Qual o valor do imóvel: '))
-salario = float(input('Qual o seu salário bruto: '))
-anos_pagar = float(input('Qtos anos pretende pagar: '))
+valor_casa = float(input('Qual o valor do imóvel:R$ '))
+salario = float(input('Qual o seu salário bruto: R$'))
+anos_pagar = int(input('Qtos anos pretende pagar: '))
 
 meses_pagar = anos_pagar * 12
 prestacao = valor_casa / meses_pagar
@@ -11,4 +11,4 @@ if nao_pode_exceder < prestacao:
     print('O valor dos 30% de seu salário é R$ {:.2f} , será destinado a  prestação do financiamento de R$ {:.2f}.'.format(nao_pode_exceder, prestacao))
     print('\033[1;31mEmpréstimo NEGADO\033[m')
 else:
-    print('\033[1;34mPARABÉNS!ACABOU DE FINANCIAR UMA CASA.\033[m A prestação \033[1:32m R$ {:.2f}.\033[m'.format(prestacao))
+    print('\033[1;34mPARABÉNS!ACABOU DE FINANCIAR UMA CASA.\033[m A prestação \033[1;32m R$ {:.2f}.\033[m'.format(prestacao))
