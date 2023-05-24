be88f3794005077a94933721accec70009a217df
flavioalessandropereira <flavioalessandropereira@gmail.com>
2023-05-24 12:12:26 -0300

    excicio 68 usando o operador ternario no print linha 17 .

--- Modulo2-PythonExercicios/ex068-jogoParouImpar.py
+++ Modulo2-PythonExercicios/ex068-jogoParouImpar.py
@@ -3,37 +3,32 @@ from random import randint
 print('=-'*20)
 print('VAMOS JOGAR PAR OU IMPAR')
 
-
-soma = 0
+total = 0
 contador = 0
+vitoria = 0
 while True:
     computador = randint(0, 10)
-    print(computador)
-
-    print('=-' * 20)
     jogador = int(input('Diga um valor: '))
-    parimpar = str(input('Par ou Ímpar?[P/I]')).strip()[0].upper()
+    total = jogador + computador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('PAR ou ÍMPAR?[P/I] ')).strip().upper()[0]
+    print(f'Vc jogou {jogador} e o computador {computador}. Total {total}. ',end='')
+    print('DEU PAR' if total % 2 == 0 else 'DEU IMPAR')
 
-    soma = jogador + computador
-    resultado = soma % 2
-
-    if parimpar == 'P':
-        if resultado == 0:
-            print('-' *20)
-            print(f'Voce jogou {jogador} e o computadador {computador}. Total de {soma} DEU PAR')
-            print('-' * 20)
-            print('Você VENCEU')
-            print('Vamos jogar novamente ...')
-
-    if parimpar == 'I':
-        if resultado != 0:
-            print('-' *20)
-            print(f'Voce jogou {jogador} e o computadador {computador}. Total de {soma} DEU IMPAR')
-            print('-' * 20)
-            print('Você VENCEU')
-            print('Vamos jogar novamente ...')
-    else:
-        print('Você PERDEU!')
-        print('=-' * 20)
-        print('GAME OVER!')
-        break
+    if tipo == 'P':
+        if total % 2 == 0:
+            print(f'Vc ganhou')
+            vitoria = vitoria + 1
+        else:
+            print('Vc perdeu!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print(f'Vc ganhou')
+            vitoria = vitoria + 1
+        else:
+            print('Vc perdeu!')
+            break
+    print('Vamos jogar novamente...')
+print(f'GAME OVER! Vc venceu {vitoria} partidas.')
