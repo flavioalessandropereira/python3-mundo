d0839f4172226cb815e3f9b01de0e4e33760bab5
flavioalessandropereira <flavioalessandropereira@gmail.com>
2023-05-25 16:05:44 -0300

    ex71 fap2

--- Modulo2-PythonExercicios/ex071-simuladorcaixaFAP.py
+++ Modulo2-PythonExercicios/ex071-simuladorcaixaFAP.py
@@ -1,2 +1,26 @@
-n = int(input('Digite o valor a sacar: R$ '))
+valor = int(input('Digite o valor a sacar: R$ '))
 
+total = valor
+
+cedulaatual = 100
+totalcedula = 0
+while True:
+    if total >= cedulaatual:
+        total = total - cedulaatual
+        totalcedula += 1
+    else:
+        if totalcedula > 0:
+             print(f'Total {totalcedula} cédulas de RS {cedulaatual}')
+        if cedulaatual == 100:
+            cedulaatual = 50
+        elif cedulaatual == 50:
+            cedulaatual = 20
+        elif cedulaatual == 20:
+            cedulaatual = 10
+        elif cedulaatual == 10:
+            cedulaatual = 5
+        elif cedulaatual == 5:
+            cedulaatual = 1
+        totalcedula = 0
+        if total == 0:
+            break
